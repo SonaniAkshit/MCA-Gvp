92b9f8633bfdd8f341de32129421e6cea11c8cf5
Akshit Patel <sonaniakshit684@gmail.com>
2024-11-03 18:47:07 +0530

    Update 1.check king nor a spade.py

--- Python/Assignment-1/1.check king nor a spade.py
+++ Python/Assignment-1/1.check king nor a spade.py
@@ -3,17 +3,8 @@ kings = 4
 spades = 13
 king_of_spades = 1
 
-king_or_spade = kings + spades - king_of_spades
-# print("\n")
-
-# print("The king OR Spade is:",king_or_spade)
-
-king_nor_spade = total_cards - king_or_spade
-
-# print("The King NOR Spade is:",king_nor_spade)
-
-pb= king_nor_spade / total_cards
+pb= 36 / total_cards
 
 print("\n")
 print(f"The Probability of it Being Neither a King NOR a Spade: {pb:.2f}")
-print("\n")
+print("\n") akshit
