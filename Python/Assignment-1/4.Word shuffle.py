7aa3e11860dd8ce42158847a75872b0acbaaf187
Akshit Patel <sonaniakshit684@gmail.com>
2024-11-22 15:47:40 +0530

    Create 4.Word shuffle.py

---
+++ Python/Assignment-1/4.Word shuffle.py
@@ -0,0 +1,10 @@
+# Word shuffle using string manipulation.
+
+print("\n")
+string=input("Enter Any String:")
+print("\n")
+print(string[2:10])
+print(string[2:11:2])
+print(string[2::-1])
+print(string[9::-1])
+print(string[::-1])
