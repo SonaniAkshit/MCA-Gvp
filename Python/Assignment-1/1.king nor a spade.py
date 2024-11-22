9e679b91e520b92f27a18733d5c458486d683889
Akshit Patel <sonaniakshit684@gmail.com>
2024-11-22 15:48:02 +0530

    Create 1.king nor a spade.py

---
+++ Python/Assignment-1/1.king nor a spade.py
@@ -0,0 +1,14 @@
+# A card is drawn at random from a deck of
+# well-shuffled cards. Find the probability of
+# it being neither a king nor a spade.
+
+total_cards = 52
+kings = 4
+spades = 13
+king_of_spades = 1
+
+pb= 36 / total_cards
+
+print("\n")
+print(f"The Probability of it Being Neither a King NOR a Spade: {pb:.2f}")
+print("\n")
